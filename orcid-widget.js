d6e976ff35c4ec9f91767732b213ad69ff00749d
leomendoza123 <leomendoza123@gmail.com>
2019-05-20 16:05:03 -0600

    Stop altering the encrypted state parameter

--- orcid-widget.js
+++ orcid-widget.js
@@ -155,12 +155,12 @@
 	}
 
 	function getQueryParameterByName(name) {
-	    name = name.replace(/[\[\]]/g, '\\$&');
-	    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-	        results = regex.exec(window.location.href);
+		name = name.replace(/[\[\]]/g, '\\$&');
+	    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+	    results = regex.exec(window.location.href);
 	    if (!results) return null;
 	    if (!results[2]) return '';
-	    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+	    return results[2];
 	}
 
 	function showAuthButton($){
